ab5bcfd05384435d959398dbf6c2b38b16bdbb84
Nicholas Powers <njpowers7915@gmail.com>
2018-08-01 16:19:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - this.startDate.getFullYear() - 1;
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    verticalDistance = Math.abs(beginningLocation.vertical - endingLocation.vertical);
+
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+  }
+
+  estimatedTime(blocksTravelled, peakHours) {
+
+  }
+}
