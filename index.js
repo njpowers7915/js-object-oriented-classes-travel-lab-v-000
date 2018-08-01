ff4c742b2ec94b869fda4828b6046563b490e749
Nicholas Powers <njpowers7915@gmail.com>
2018-08-01 16:03:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Driver {
   }
 
   yearsExperienceFromBeginningOf(endDate) {
-    return endDate - this.startDate.getFullYear() + 1;
+    return endDate - this.startDate.getFullYear() - 1;
   }
 }
 
